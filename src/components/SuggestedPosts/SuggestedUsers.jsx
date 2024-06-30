3401b49e9604dea9b5d728641d6e5327b1c6f7fc
MVARUNREDDY8203 <varunmr2003@gmail.com>
2024-06-30 12:08:23 +0530

    minor ui change

--- src/components/SuggestedPosts/SuggestedUsers.jsx
+++ src/components/SuggestedPosts/SuggestedUsers.jsx
@@ -36,7 +36,7 @@ const SuggestedUsers = () => {
             ))}
 
             <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-                © 2023 Built By{" "}
+                © 2024 Built By{" "}
                 <Link
                     href='https://github.com/MVARUNREDDY8203'
                     target='_blank'
